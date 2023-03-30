80db6e1ef6a91836c83ccb1c0d47a15556de8e1b
Prachi <sachdevaprachi6@gmail.com>
2023-03-30 22:22:09 +0530

    implement posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,37 @@
+import{useState,useEffect} from'react';
+import {firestore} from '../firebase';
+import {Link} from 'react-router-dom';
 function Home() {
+  const[posts,setPosts]=useState([]);
+  useEffect(()=>{
+   firestore.collection('posts').get().then((snapshot)=>{
+    const posts=snapshot.docs.map((doc)=>{
+      // console.log(doc.data());
+      // console.log(doc.id);
+      // console.log(doc);
+      return {
+       id:doc.id,
+       ...doc.data()
+      };
+    });
+    console.log('posts',posts);
+    setPosts(posts);
+   });
+  },[]);
   return (
-    <div className="">
-      Home
+    <div className="home">
+      <h1>Tech Blog</h1>
+      <div id="blog-by">Prachi</div>
+
+        {posts.map((post,index)=>(
+         <div className='post' key={`post-${index}`}>
+          <Link to={`/post/${post.id}`}>
+            <h3>{post.title}</h3>
+          </Link>
+          <p>{post.subTitle}</p>
+         </div>
+        ))}
+      
      
     </div>
   );
